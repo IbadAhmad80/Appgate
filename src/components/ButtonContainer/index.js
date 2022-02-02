42a11627503c9189fe71e2404c8f037e93fe354a
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-02-02 22:04:59 +0500

    Add initial files

--- src/components/ButtonContainer/index.js
+++ src/components/ButtonContainer/index.js
@@ -6,7 +6,7 @@ const Button = (props) => {
   return (
     <div className={styles.button_container}>
       <h4>{props.text}</h4>
-      <img src={play_icon} alt="play icon" />
+      <img src={play_icon} alt="play icon " />
     </div>
   );
 };
